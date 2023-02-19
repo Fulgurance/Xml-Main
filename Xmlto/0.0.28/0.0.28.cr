a0fcbb2c857c8f767e6937d1fd2cdf3768cba3c0
fulgurance <zohran.londais@gmail.com>
2023-02-19 12:52:41 +0000

    Added Xmlto installer

---
+++ Xmlto/0.0.28/0.0.28.cr
@@ -0,0 +1,20 @@
+class Target < ISM::Software
+    
+    def configure
+        super
+        configureSource([   "--prefix=/usr"],
+                            buildDirectoryPath,
+                            {"LINKS" => "/usr/bin/links"})
+    end
+    
+    def build
+        super
+        makeSource([Ism.settings.makeOptions],buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+        makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+    end
+
+end
