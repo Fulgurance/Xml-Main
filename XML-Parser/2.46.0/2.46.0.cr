fa2372137712e5ed636a41c6255bd0593daf1c73
fulgurance <zohran.londais@gmail.com>
2023-03-29 12:11:46 +0100

    Patched wrong argument

--- XML-Parser/2.46.0/2.46.0.cr
+++ XML-Parser/2.46.0/2.46.0.cr
@@ -2,16 +2,19 @@ class Target < ISM::Software
     
     def prepare
         super
-        makePerlSource(buildDirectoryPath)
+
+        makePerlSource(path: buildDirectoryPath)
     end
     
     def build
         super
+
         makeSource([Ism.settings.makeOptions],buildDirectoryPath)
     end
     
     def prepareInstallation
         super
+
         makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
     end
 
