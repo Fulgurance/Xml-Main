0daed5ba635ef52ab2cd60518e10118ce09413a1
Fulgurance <zohran.londais@gmail.com>
2023-04-21 15:08:33 +0100

    Patched expat

--- Expat/2.4.1/2.4.1.cr
+++ Expat/2.4.1/2.4.1.cr
@@ -10,12 +10,12 @@ class Target < ISM::Software
 
     def build
         super
-        makeSource([Ism.settings.makeOptions],buildDirectoryPath)
+        makeSource(path: buildDirectoryPath)
     end
 
     def prepareInstallation
         super
-        makeSource([Ism.settings.makeOptions,"DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
     end
 
 end
