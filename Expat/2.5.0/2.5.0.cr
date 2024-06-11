3b403e1b08102198426252d0fa7cc25513bad79a
Fulgurance <zohran.londais@gmail.com>
2024-06-11 22:19:38 +0100

    Updated accessor name

--- Expat/2.5.0/2.5.0.cr
+++ Expat/2.5.0/2.5.0.cr
@@ -24,7 +24,7 @@ class Target < ISM::Software
                             path: buildDirectoryPath(entry: "MainBuild"))
 
         if option("32Bits")
-            configureSource([   "--host=i686-#{Ism.settings.targetName}-linux-gnu",
+            configureSource([   "--host=i686-#{Ism.settings.systemTargetName}-linux-gnu",
                                 "--prefix=/usr",
                                 "--libdir=/usr/lib32",
                                 "--disable-static"],
@@ -33,7 +33,7 @@ class Target < ISM::Software
         end
 
         if option("x32Bits")
-            configureSource([   "--host=#{Ism.settings.target}x32",
+            configureSource([   "--host=#{Ism.settings.systemTarget}x32",
                                 "--prefix=/usr",
                                 "--libdir=/usr/libx32",
                                 "--disable-static"],
