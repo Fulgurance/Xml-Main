fe78f35f4c8b74d5aaf4b93557b537796886c3b3
Fulgurance <zohran.londais@gmail.com>
2025-04-29 13:55:02 +0100

    Setted target triple

--- Expat/2.5.0/2.5.0.cr
+++ Expat/2.5.0/2.5.0.cr
@@ -3,8 +3,11 @@ class Target < ISM::Software
     def configure
         super
 
-        configureSource(arguments:  "--prefix=/usr      \
-                                    --disable-static    \
+        configureSource(arguments:  "--prefix=/usr                          \
+                                    --host=#{Ism.settings.systemTarget}     \
+                                    --build=#{Ism.settings.systemTarget}    \
+                                    --target=#{Ism.settings.systemTarget}   \
+                                    --disable-static                        \
                                     --docdir=/usr/share/doc/expat-2.5.0",
                         path: buildDirectoryPath)
     end
